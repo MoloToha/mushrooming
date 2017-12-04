3cb6c50785f2b74b344695275e5488b2b72f5564
Konrad <konrad.werblinski@gmail.com>
2017-12-04 21:26:01 +0100

    Added unit tests for Team and User classes

--- app/src/test/java/com/example/antonl/mushrooming/TeamUnitTest.java
+++ app/src/test/java/com/example/antonl/mushrooming/TeamUnitTest.java
@@ -2,13 +2,15 @@ package com.example.antonl.mushrooming;
 
 import com.mushrooming.base.Position;
 import com.mushrooming.base.Team;
-
+import com.mushrooming.base.User;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -23,6 +25,41 @@ public class TeamUnitTest {
         team.updateUser(1, new Position(0,0));
         team.updateUser(2, new Position(0,0));
 
+        assertEquals(2, team.getUsers().size());
 
+        team.removeUser(1);
+        team.removeUser(2);
+
+        assertTrue(team.getUsers().isEmpty());
     }
+
+    @Test
+    public void teamManaging2() throws Exception {
+        Team team = new Team();
+        team.addUser(new User(42, new Position(4, 2)));
+        team.addUser(new User(44, new Position(4, 4)));
+        for (User u : team.getUsers())
+            assertTrue(u.getGpsPosition().getX() == 4);
+
+        team.updateUser(42, new Position(2, 2));
+
+        int count = 0;
+        for (User u : team.getUsers())
+            if (u.getGpsPosition().getX() == 4)
+                count++;
+
+        assertEquals(1, count);
+
+        team.updateUser(44, new Position(3,3));
+
+        count = 0;
+        for (User u : team.getUsers())
+            assertFalse(u.getGpsPosition().getX() == 4);
+
+        team.removeUser(44);
+        team.removeUser(42);
+        team.removeUser(3);
+        team.removeUser(14);
+    }
+
 }
--- app/src/test/java/com/example/antonl/mushrooming/UserUnitTest.java
+++ app/src/test/java/com/example/antonl/mushrooming/UserUnitTest.java
@@ -3,6 +3,7 @@ package com.example.antonl.mushrooming;
 import org.junit.Test;
 import com.mushrooming.base.Position;
 import com.mushrooming.base.User;
+import java.lang.reflect.*;
 
 import static org.junit.Assert.*;
 
@@ -21,5 +22,34 @@ public class UserUnitTest {
         assertEquals(u1, u2);
         assertNotEquals(u1, u3);
         assertNotEquals(u2, u3);
+        assertNotEquals(u1, "Meddle");
     }
+
+    @Test
+    public void userIsConnected()  throws Exception {
+        User u = new User(1, new Position(1,1));
+        assertTrue(u.isConnected());
+
+        Field f = u.getClass().getDeclaredField("_lastUpdate");
+        f.setAccessible(true);
+        f.setLong(u, (long)f.get(u) - (long)10001);
+
+        assertFalse(u.isConnected());
+
+        u.update(new Position(5,5));
+        assertTrue(u.isConnected());
+    }
+
+    @Test
+    public void userPosAndID()  throws Exception {
+        User u = new User(66, new Position(666,7734));
+        assertEquals(66, u.getId());
+        assertTrue(666 == u.getGpsPosition().getX());
+        assertTrue(7734 ==  u.getGpsPosition().getY());
+
+        u.update(new Position(59,42));
+        assertTrue(59 == u.getGpsPosition().getX());
+        assertTrue(42 ==  u.getGpsPosition().getY());
+    }
+
 }
