dffa0baa41f7b940c54706949914b2350ce0eb57
Konrad <konrad.werblinski@gmail.com>
2017-12-12 11:25:26 +0100

    TeamUnitTest refactor

--- app/src/test/java/com/example/antonl/mushrooming/TeamUnitTest.java
+++ app/src/test/java/com/example/antonl/mushrooming/TeamUnitTest.java
@@ -19,7 +19,7 @@ import static org.junit.Assert.assertTrue;
  */
 public class TeamUnitTest {
     @Test
-    public void teamManaging() throws Exception {
+    public void simpleTeamManaging() throws Exception {
         Team team = new Team();
 
         team.updateUser(1, new Position(0,0));
@@ -34,32 +34,37 @@ public class TeamUnitTest {
     }
 
     @Test
-    public void teamManaging2() throws Exception {
+    public void teamManaging() throws Exception {
         Team team = new Team();
         team.addUser(new User(42, new Position(4, 2)));
         team.addUser(new User(44, new Position(4, 4)));
-        for (User u : team.getUsers())
+        for (User u : team.getUsers()) {
             assertTrue(u.getGpsPosition().getX() == 4);
+        }
 
         team.updateUser(42, new Position(2, 2));
 
         int count = 0;
-        for (User u : team.getUsers())
-            if (u.getGpsPosition().getX() == 4)
+        for (User u : team.getUsers()) {
+            if (u.getGpsPosition().getX() == 4) {
                 count++;
+            }
+        }
 
         assertEquals(1, count);
 
         team.updateUser(44, new Position(3,3));
 
-        count = 0;
-        for (User u : team.getUsers())
+        for (User u : team.getUsers()) {
             assertFalse(u.getGpsPosition().getX() == 4);
+        }
 
         team.removeUser(44);
         team.removeUser(42);
         team.removeUser(3);
         team.removeUser(14);
+
+        assertTrue(team.getUsers().isEmpty());
     }
 
 }
